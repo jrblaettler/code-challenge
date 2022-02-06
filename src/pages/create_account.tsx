a50a390f76c9fd17619a80b0606a4f54337a1356
Jacob Blaettler <jrblaettler@gmail.com>
2022-02-06 15:46:45 -0700

    Changes committed:
    	new file:   src/assets/logo.png
    	modified:   src/pages/create_account.tsx
    	modified:   src/styles/create_account.module.scss

--- src/pages/create_account.tsx
+++ src/pages/create_account.tsx
@@ -1,6 +1,8 @@
 import Head from 'next/head';
 import { FormEvent, ChangeEvent, useState } from 'react';
 import styles from 'src/styles/create_account.module.scss';
+import Image from 'next/image';
+import logo from '../assets/logo.png';
 
 export default function CreateAccount() {
   const [username, setUsername] = useState('');
@@ -22,11 +24,17 @@ export default function CreateAccount() {
         }),
       });
       const resBody = await response.json();
-      if (resBody.errors.username === 'fail') {
+      if (resBody.errors?.username === 'fail') {
         setIsUsernameError(true);
       }
-      if (resBody.errors.password === 'fail') {
+      if (resBody.errors?.password === 'fail') {
         setIsPasswordError(true);
+      }
+      if (resBody.result === true) {
+        console.log('Account Created');
+        setIsPasswordError(false);
+        setIsUsernameError(false);
+        setIsPasswordCracked(false);
       }
     } catch (err) {
       console.log('Error:', err);
@@ -47,57 +55,73 @@ export default function CreateAccount() {
     return resBody.result;
   };
 
-  const handleUserChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setUsername(e.currentTarget.value);
-  };
-
-  const handlePasswordChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setPassword(e.currentTarget.value);
+  const handleTextChange = (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.currentTarget.name === 'username') {
+      setUsername(e.currentTarget.value);
+    }
+    if (e.currentTarget.name === 'password') {
+      setPassword(e.currentTarget.value);
+    }
   };
 
   return (
     <>
       <Head>
         <title>Create Account</title>
+        <link
+          href='https://fonts.googleapis.com/css2?family=Roboto&display=swap'
+          rel='stylesheet'
+        ></link>
       </Head>
       <article className={styles.article}>
         <form className={styles.form} onSubmit={handleSubmit}>
-          <h1>Create New Account</h1>
-          <label htmlFor='username'>Username</label>
-          <input
-            className={styles.input}
-            name='username'
-            type='text'
-            placeholder='username'
-            onChange={e => handleUserChange(e)}
-            value={username}
-          />
-          {isUsernameError ? (
-            <p>Username must be between 10 and 50 characters</p>
-          ) : null}
-          <label htmlFor='password'>Password</label>
-          <input
-            className={styles.input}
-            name='password'
-            type='password'
-            placeholder='password'
-            onChange={e => handlePasswordChange(e)}
-            value={password}
-          />
-          {isPasswordCracked ? (
-            <p>
-              This password has been hacked elsewhere, choose a different one
-            </p>
-          ) : null}
-          {isPasswordError ? (
-            <ul>
-              Password must:
-              <li>be between 20 and 50 characters</li>
-              <li>contain at least one symbol (!,@,#,$,%)</li>
-              <li>contain at least one uppercase or lowercase letter</li>
-              <li>contain at least one number(0-9)</li>
-            </ul>
-          ) : null}
+          <Image src={logo} width={75} height={65} />
+          <h1 className={styles.title}>Create New Account</h1>
+          <div className={styles.input_container}>
+            <label className={styles.label} htmlFor='username'>
+              Username
+            </label>
+            <input
+              className={styles.input}
+              name='username'
+              type='text'
+              onChange={e => handleTextChange(e)}
+              value={username}
+            />
+            {isUsernameError ? (
+              <p className={styles.error}>
+                Username must be between 10 and 50 characters
+              </p>
+            ) : null}
+          </div>
+          <div className={styles.input_container}>
+            <label className={styles.label} htmlFor='password'>
+              Password
+            </label>
+            <input
+              className={styles.input}
+              name='password'
+              type='password'
+              onChange={e => handleTextChange(e)}
+              value={password}
+            />
+            {isPasswordCracked ? (
+              <p className={styles.error}>
+                This password has been hacked elsewhere, choose a different one
+              </p>
+            ) : null}
+            {isPasswordError ? (
+              <>
+                <p className={styles.error}>Password must:</p>
+                <ul className={styles.error}>
+                  <li>be between 20 and 50 characters</li>
+                  <li>contain at least one symbol (!,@,#,$,%)</li>
+                  <li>contain at least one uppercase or lowercase letter</li>
+                  <li>contain at least one number(0-9)</li>
+                </ul>
+              </>
+            ) : null}
+          </div>
           <button className={styles.button} type='submit'>
             Create Account
           </button>
