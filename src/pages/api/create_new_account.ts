77d3326adb0aa188f1cfe99336a701fcab6e3f5b
Jacob Blaettler <jrblaettler@gmail.com>
2022-02-06 13:14:27 -0700

    Changes committed:
    	modified:   package-lock.json
    	modified:   src/pages/api/create_new_account.ts
    	modified:   src/pages/create_account.tsx

--- src/pages/api/create_new_account.ts
+++ src/pages/api/create_new_account.ts
@@ -10,6 +10,34 @@ interface BooleanResult {
   errors?: Record<string, string>;
 }
 
-export default function createNewAccount(req: NextApiRequest, res: NextApiResponse<BooleanResult>) {
-  res.status(200).json({ result: true });
+export default function createNewAccount(
+  req: NextApiRequest,
+  res: NextApiResponse<BooleanResult>
+) {
+  const newUser: CreateNewAccountParameters = JSON.parse(req.body);
+  const userParams = new RegExp('^(?=.{10,50})');
+  const passwordParams = new RegExp(
+    '^(?=.{20,50})(?=.*[A-Za-z])(?=.*[!@#$%])(?=.*[0-9])'
+  );
+  if (
+    !newUser.username.match(userParams) &&
+    !newUser.password.match(passwordParams)
+  ) {
+    res.status(400).json({
+      result: false,
+      errors: { username: 'fail', password: 'fail' },
+    });
+  } else if (!newUser.username.match(userParams)) {
+    res.status(400).json({
+      result: false,
+      errors: { username: 'fail', password: 'pass' },
+    });
+  } else if (!newUser.password.match(passwordParams)) {
+    res.status(400).json({
+      result: false,
+      errors: { username: 'pass', password: 'fail' },
+    });
+  } else {
+    res.status(200).json({ result: true });
+  }
 }
--- src/pages/create_account.tsx
+++ src/pages/create_account.tsx
@@ -1,17 +1,31 @@
 import Head from 'next/head';
-import { FormEvent } from 'react';
+import { FormEvent, ChangeEvent, useState } from 'react';
 import styles from 'src/styles/create_account.module.scss';
 
 export default function CreateAccount() {
+  const [username, setUsername] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
   async function handleSubmit(evt: FormEvent) {
     evt.preventDefault();
     const response = await fetch('/api/create_new_account', {
       method: 'POST',
-      body: JSON.stringify({}),
+      body: JSON.stringify({
+        username,
+        password,
+      }),
     });
 
     console.log(await response.json());
   }
+
+  const handleUserChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setUsername(e.currentTarget.value);
+  };
+
+  const handlePasswordChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.currentTarget.value);
+  };
 
   return (
     <>
@@ -20,6 +34,23 @@ export default function CreateAccount() {
       </Head>
       <article className={styles.article}>
         <form className={styles.form} onSubmit={handleSubmit}>
+          <h1>Create New Account</h1>
+          <label htmlFor='username'>Username</label>
+          <input
+            name='username'
+            type='text'
+            placeholder='username'
+            onChange={e => handleUserChange(e)}
+            value={username}
+          />
+          <label htmlFor='password'>Password</label>
+          <input
+            name='password'
+            type='password'
+            placeholder='password'
+            onChange={e => handlePasswordChange(e)}
+            value={password}
+          />
           <button>Create Account</button>
         </form>
       </article>
