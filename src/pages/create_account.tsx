f42ad12b0b8439a9b1713348dce30054557b0ad8
Jacob Blaettler <jrblaettler@gmail.com>
2022-02-09 02:57:05 -0700

    Changes committed:
    	modified:   src/pages/create_account.tsx

--- src/pages/create_account.tsx
+++ src/pages/create_account.tsx
@@ -39,7 +39,7 @@ export default function CreateAccount() {
         <link
           href='https://fonts.googleapis.com/css2?family=Roboto&display=swap'
           rel='stylesheet'
-        ></link>
+        />
       </Head>
       <article className={styles.article}>
         <div className={styles.form_container}>
